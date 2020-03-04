dd1976ec8cd48afe569cc84e194ffe18347d58b6
Simon1002 <simon.sander@student.ecp.fr>
2020-03-04 11:07:19 +0100

    Notebook updated

--- data/extraction_twint/extraction_tweets.py
+++ data/extraction_twint/extraction_tweets.py
@@ -146,7 +146,7 @@ def get_unique(a_list):
 if __name__=="__main__":
 
     #extract_service_clients_data(1, 200)
-    extract_marketing_personnalise_data(1, 100)
+    extract_marketing_personnalise_data(3, 100)
 
 
 
